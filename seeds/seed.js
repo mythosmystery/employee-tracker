6bdaaa005051ba20d9f4001717d6d4e67343f1be
mythosmystery <axiysfire@gmail.com>
2021-05-17 03:50:27 -0500

    added seeding functionality

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,79 +1,17 @@
 const { Role, Employee, Department } = require("../models");
+const sequelize = require("../lib/connection");
 
-const seedDepartments = async () => {
-    await Department.bulkCreate([
-        {
-            name: "Managment"
-        },
-        {
-            name: "System Administration"
-        },
-        {
-            name: "Software Development"
-        },
-        {
-            name: "Customer Service"
-        }
-    ]);
-}
-const seedRoles = async () => {
-    await Role.bulkCreate([
-        {
-            title: "Manager",
-            salary: 100000,
-            department_id: 1
-        },
-        {
-            title: "Analyst",
-            salary: 80000,
-            department_id: 2
-        },
-        {
-            title: "Senior Engineer",
-            salary: 200000,
-            department_id: 3
-        },
-        {
-            title: "Junior Engineer",
-            salary: 65000,
-            department_id: 3
-        },
-        {
-            title: "Customer Service Rep",
-            salary: 45000,
-            department_id: 4
-        }
-    ]);
-}
-const seedEmployees = async () => {
-    await Employee.bulkCreate([
-        {
-            first_name: "Jim",
-            last_name: "Lahey",
-            role_id: 1,
-            manager_id: 2
-        },
-        {
-            first_name: "Randy",
-            last_name: "Bo-bandy",
-            role_id: 2,
-            manager_id: 1
-        },
-        {
-            first_name: "Ricky",
-            last_name: "LaFluer",
-            role_id: 1,
-            manager_id: 1
-        },
-        {
-            first_name: "Bubbles",
-            last_name: "",
-            role_id: 4,
-            manager_id: 3
-        }
-    ]);
+const employeeJson = require("./employeeJson.json")
+const roleJson = require("./roleJson.json")
+const departmentJson = require("./departmentJson.json")
+
+const seed = async () => {
+    await sequelize.drop();
+    await sequelize.sync()
+    await Department.bulkCreate(departmentJson);
+    await Role.bulkCreate(roleJson);
+    await Employee.bulkCreate(employeeJson);
+    console.log("Seeding complete!")
 }
 
-seedDepartments();
-seedRoles();
-seedEmployees();
+seed();
